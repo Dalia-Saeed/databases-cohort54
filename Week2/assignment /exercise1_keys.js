f6fa371d06f69cd8fe71948fa0f3d06a2bbf1213
Dalia-Saeed <dalisaeed09@gmail.com>
2025-12-19 12:26:29 +0100

    assignment-w2

--- Week2/assignment /exercise1_keys.js
+++ Week2/assignment /exercise1_keys.js
@@ -1,44 +1,25 @@
 import { client } from "./assignment/db_config.js";
 
-
 async function run() {
-    await client.connect();
+  await client.connect();
 
-    await client.query(`
-        CREATE TABLE IF NOT EXISTS authors (
-    author_id SERIAL PRIMARY KEY,
-    author_name VARCHAR(255) NOT NULL,
-    date_of_birth DATE NOT NULL,
-    university VARCHAR(255),
-    h_index INT,
-    gender VARCHAR(50)
-);
-
-    `);
-
-    await client.query(`
-        ALTER TABLE authors
-        ADD COLUMN IF NOT EXISTS mentor INT;
-    `);
-
-    await client.query(`
-       DO $$
-BEGIN
-    IF NOT EXISTS (
-        SELECT 1 FROM pg_constraint
-        WHERE conname = 'fk_author_mentor'
-    ) THEN
-        ALTER TABLE authors
-        ADD CONSTRAINT fk_author_mentor
+  await client.query(`
+    CREATE TABLE authors (
+      author_id SERIAL PRIMARY KEY,
+      author_name VARCHAR(255) NOT NULL,
+      date_of_birth DATE NOT NULL,
+      university VARCHAR(255),
+      h_index INT,
+      gender VARCHAR(50),
+      mentor INT,
+      CONSTRAINT fk_author_mentor
         FOREIGN KEY (mentor)
-        REFERENCES authors(author_id);
-    END IF;
-END $$;
+        REFERENCES authors(author_id)
+    );
+  `);
 
-    `);
-
-    console.log("Exercise 1: authors table created with mentor column.");
-    await client.end();
+  console.log("Exercise 1: authors table created with mentor column.");
+  await client.end();
 }
 
 run().catch(console.error);
