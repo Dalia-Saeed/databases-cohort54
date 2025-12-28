83d8fd49b71b0d38a1e22c7d897ad1bc7b2c440d
Dalia-Saeed <dalisaeed09@gmail.com>
2025-12-28 02:21:19 +0100

    for screen

--- Week3/assignment3/exercise4/index.js
+++ Week3/assignment3/exercise4/index.js
@@ -1,58 +1,143 @@
-import 'dotenv/config';
-import { MongoClient } from "mongodb";
-import seedDatabase from "./seedDatabase.js";
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const { seedDatabase } = require("./seedDatabase.js");
+
+async function createEpisodeExercise(client) {
+  // Add season 9 episode 13
+  const newEpisode = {
+    episode: "S09E13",
+    title: "MOUNTAIN HIDE-AWAY",
+    elements: [
+      "CIRRUS", "CLOUDS", "CONIFER", "DECIDIOUS", "GRASS",
+      "MOUNTAIN", "MOUNTAINS", "RIVER", "SNOWY_MOUNTAIN",
+      "TREE", "TREES"
+    ]
+  };
+  const result = await client
+    .db("databaseWeek3")
+    .collection("bob_ross_episodes")
+    .insertOne(newEpisode);
+
+  console.log(
+    `Created season 9 episode 13 and the document got the id ${result.insertedId}`
+  );
+}
+
+async function findEpisodesExercises(client) {
+  const collection = client.db("databaseWeek3").collection("bob_ross_episodes");
+
+  // Title of episode 2 in season 2
+  const ep2S2 = await collection.findOne({ episode: "S02E02" });
+  console.log(`The title of episode 2 in season 2 is ${ep2S2.title}`);
+
+  // Episode called BLACK RIVER
+  const blackRiver = await collection.findOne({ title: "BLACK RIVER" });
+  console.log(
+    `The season and episode number of the "BLACK RIVER" episode is ${blackRiver.episode}`
+  );
+
+  // All episodes where Bob Ross painted a CLIFF
+  const cliffEpisodes = await collection
+    .find({ elements: "CLIFF" })
+    .toArray();
+  console.log(
+    `The episodes that Bob Ross painted a CLIFF are ${cliffEpisodes
+      .map((e) => e.title)
+      .join(", ")}`
+  );
+
+  // All episodes where Bob Ross painted both a CLIFF and a LIGHTHOUSE
+  const cliffLighthouseEpisodes = await collection
+    .find({ elements: { $all: ["CLIFF", "LIGHTHOUSE"] } })
+    .toArray();
+  console.log(
+    `The episodes that Bob Ross painted a CLIFF and a LIGHTHOUSE are ${cliffLighthouseEpisodes
+      .map((e) => e.title)
+      .join(", ")}`
+  );
+}
+
+async function updateEpisodeExercises(client) {
+  const collection = client.db("databaseWeek3").collection("bob_ross_episodes");
+
+  // Correct the title of episode S30E13
+  const updateEpisode = await collection.updateOne(
+    { episode: "S30E13" },
+    { $set: { title: "BLUE RIDGE FALLS" } }
+  );
+  console.log(
+    `Ran a command to update episode 13 in season 30 and it updated ${updateEpisode.modifiedCount} episodes`
+  );
+
+  // Change all elements labeled BUSHES to BUSH
+  const updateBushes = await collection.updateMany(
+    { elements: "BUSHES" },
+    { $set: { "elements.$": "BUSH" } }
+  );
+  console.log(
+    `Ran a command to update all the BUSHES to BUSH and it updated ${updateBushes.modifiedCount} episodes`
+  );
+}
+
+async function deleteEpisodeExercise(client) {
+  const collection = client.db("databaseWeek3").collection("bob_ross_episodes");
+
+  // Delete episode S31E14
+  const deleteResult = await collection.deleteOne({ episode: "S31E14" });
+  console.log(
+    `Ran a command to delete episode and it deleted ${deleteResult.deletedCount} episodes`
+  );
+}
+
+async function main() {
+  if (!process.env.MONGODB_URL) {
+    throw Error(
+      "You did not set up the environment variables correctly. Please create a '.env' file."
+    );
+  }
+
+  const client = new MongoClient(process.env.MONGODB_URL, {
+  serverApi: ServerApiVersion.v1,
+});
 
 
-const uri = process.env.MONGODB_URI;
-const dbName = process.env.DB_NAME;
-
-const client = new MongoClient(uri);
-
-async function run() {
   try {
-    // Connect to MongoDB
     await client.connect();
-    console.log("Connected to MongoDB Atlas");
 
-    const db = client.db(dbName);
-    const collection = db.collection("bob_ross_episodes");
-
-    // Seed database
-    await seedDatabase(db);
-    console.log("Database seeded");
+    // Seed the database
+    await seedDatabase(client);
 
     // CREATE
-    const createResult = await collection.insertOne({
-      title: "My Happy Trees",
-      elements: ["trees", "mountain", "river"]
-    });
-    console.log("Inserted document with id:", createResult.insertedId);
+    await createEpisodeExercise(client);
 
     // READ
-    const episodesWithTrees = await collection
-      .find({ elements: "trees" })
-      .toArray();
-    console.log("Number of episodes with trees:", episodesWithTrees.length);
+    await findEpisodesExercises(client);
 
     // UPDATE
-    const updateResult = await collection.updateOne(
-      { title: "My Happy Trees" },
-      { $set: { title: "My Even Happier Trees" } }
-    );
-    console.log("Number of updated documents:", updateResult.modifiedCount);
+    await updateEpisodeExercises(client);
 
     // DELETE
-    const deleteResult = await collection.deleteOne({
-      title: "My Even Happier Trees"
-    });
-    console.log("Number of deleted documents:", deleteResult.deletedCount);
-
+    await deleteEpisodeExercise(client);
   } catch (err) {
-    console.error("Error:", err);
+    console.error(err);
   } finally {
     await client.close();
-    console.log("Connection closed");
   }
 }
 
-run();
+main();
+
+
+/**
+ * In the end the console should read something like this: 
+
+Created season 9 episode 13 and the document got the id 625e9addd11e82a59aa9ff93
+The title of episode 2 in season 2 is WINTER SUN
+The season and episode number of the "BLACK RIVER" episode is S02E06
+The episodes that Bob Ross painted a CLIFF are NIGHT LIGHT, EVENING SEASCAPE, SURF'S UP, CLIFFSIDE, BY THE SEA, DEEP WILDERNESS HOME, CRIMSON TIDE, GRACEFUL WATERFALL
+The episodes that Bob Ross painted a CLIFF and a LIGHTHOUSE are NIGHT LIGHT
+Ran a command to update episode 13 in season 30 and it updated 1 episodes
+Ran a command to update all the BUSHES to BUSH and it updated 120 episodes
+Ran a command to delete episode and it deleted 1 episodes
+ 
+*/
--- Week3/assignment3/exercise4/seedDatabase.js
+++ Week3/assignment3/exercise4/seedDatabase.js
@@ -1,16 +1,51 @@
-import data from "./data.json" assert { type: "json" };
+const data = require("./data.json");
 
-export default async function seedDatabase(db) {
-  const collection = db.collection(process.env.COLLECTION);
+/**
+ * This function will drop and recreate the collection of sample data in our csv file.
+ * By doing this we ensure that your functions are working on the same data, very similar to how you would set up a test environment.
+ *
+ * @param {MongoClient} client - The client that is connected to your database
+ */
+const seedDatabase = async (client) => {
+  const hasCollection = await client
+    .db("databaseWeek3")
+    .listCollections({ name: "bob_ross_episodes" })
+    .hasNext();
 
-  await collection.deleteMany({});
-  await collection.insertMany(
-    data.map((ep, idx) => ({
-      episode: idx + 1,
-      title: ep.title,
-      elements: ep.elements
-    }))
-  );
+  if (hasCollection) {
+    const bobRossCollection = await client
+      .db("databaseWeek3")
+      .collection("bob_ross_episodes");
 
-  console.log("Database seeded.");
-}
+    // Remove all the documents
+    await bobRossCollection.deleteMany({});
+
+    // Convert data to array version of elements
+    const documents = data.map((dataItem) => {
+      const { EPISODE, TITLE } = dataItem;
+
+      const depictionElementKeys = Object.keys(dataItem).filter(
+        (key) => !["EPISODE", "TITLE"].includes(key)
+      );
+      const depictionElements = depictionElementKeys.filter(
+        (key) => dataItem[key] === 1
+      );
+
+      return {
+        episode: EPISODE,
+        // Remove the extra quotation marks
+        title: TITLE.replaceAll('"', ""),
+        elements: depictionElements,
+      };
+    });
+
+    // Add our documents
+    await bobRossCollection.insertMany(documents);
+  } else {
+    throw Error("The collection `bob_ross_episodes` does not exist!");
+  }
+};
+
+module.exports = {
+  seedDatabase,
+};
